f2f379fabcab2d6d5296ee7136bd397dc4752b40
roberte <robertonsate23@gmail.com>
2026-04-14 00:08:46 +0300

    analyzing data with module and syntax

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+for (const array of args) {
+  combinedObject.users = [...combinedObject.users, ...array];
+}
+
+combinedObject.merge_date =Date.today().toString("M/d/yyyy");
+return combinedObject;
+}
 
 
 
